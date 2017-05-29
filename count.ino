398d66c6f47afc38a32181fedcbdf7fd2ca237e5
ganjanggongjang <ganjanggongjang@gmail.com>
2017-05-30 01:15:56 +0900

    people counter 최종

--- count.ino
+++ count.ino
@@ -1,8 +1,11 @@
+#include <Time.h>
+#include <TimeLib.h>
+
 
 const char* host = "api.thingspeak.com";
 String url = "/update?api_key=3GOJDFXCK0U4I6R3";  // Your Own Key here
 const int httpPort = 80;
-int interval = 500;
+int interval = 1500;
 #define calibration_factor -1500.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
 
 #include <ESP8266WiFi.h>
@@ -12,7 +15,7 @@ const char* ssid = "ktEgg_493";  // Your own ssid here
 const char* password = "info22093"; // Your own password here
 char scaleString[6];
 float currentWeight = 0;
-const char* private_server = "163.239.78.93";
+const char* private_server = "52.78.118.210";
 const int serverPort = 3000;
 
 HX711 scale(D2,D3);
@@ -21,6 +24,7 @@ unsigned long start = 0;
 unsigned long finish=0;
 int flag=0;
 float people=0;
+float hour_people=0;
 String working(float data) {
   if (data>30)
   {
@@ -39,25 +43,7 @@ return String(scaleString);
 
 void delivering(String payload) {
   WiFiClient client;
-  Serial.print("connecting to ");
-  Serial.println(host);
-  if (!client.connect(host, httpPort)) {
-    Serial.print("connection failed: ");
-    Serial.println(payload);
-    return;
-  }
-  String getheader = "GET "+ String(url) +"&"+ String(payload) +" HTTP/1.1";
-  client.println(getheader);
-  client.println("User-Agent: ESP8266");
-  client.println("Host: " + String(host));
-  client.println("Connection: close");
-  client.println();
-  
-  Serial.println(getheader);
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    Serial.println(line);
-    }
+
 
 Serial.print("\nconnecting to Host: ");
   Serial.println(private_server);
@@ -73,10 +59,10 @@ Serial.print("\nconnecting to Host: ");
     client.println();
   
     Serial.println(getheader);//To Check
-    while (client.connected()) {
+  /*  while (client.connected()) {
       String line = client.readStringUntil('\n');
       Serial.println(line);
-    }
+    }*/
   }
   else{
     Serial.print("connection failed to ");
@@ -108,10 +94,16 @@ void setup() {
 }
 unsigned long mark = 0;
 void loop() {
-  if (millis() > mark ) {
+  if(hour()==23 && minute()==59&&second()>55)
+  {
+      people=0;    
+  }
+  if (millis() > mark) {
+    mark+=interval;
     String payload = working(scale.get_units());
-    delivering(payload);
+    //if(minute()%5==0&&second()==0)
+     if(second()%30==0)
+      delivering(payload);
     }
 }
-
 
