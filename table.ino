8b41970536f65050db9a2db8daf038389dc150c0
ganjanggongjang <ganjanggongjang@gmail.com>
2017-05-30 01:14:08 +0900

    Add files via upload

--- table.ino
+++ table.ino
@@ -1,10 +1,10 @@
-
-
+#include <Time.h>
+#include <TimeLib.h>
 
 
 const char* host = "api.thingspeak.com";
 String url = "/update?api_key=3GOJDFXCK0U4I6R3";  // Your Own Key here
-const int httpPort = 80;
+const int httpPort = 8080;
 int interval = 500;
 #define calibration_factor -1500.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
 
@@ -15,12 +15,16 @@ const char* ssid = "ktEgg_493";  // Your own ssid here
 const char* password = "info22093"; // Your own password here
 char scaleString[6];
 float currentWeight = 0;
+const char* private_server = "52.78.118.210";
+const int serverPort = 8080;
 
 HX711 scale(D2,D3);
 unsigned long tabletime =0;
 unsigned long start = 0;
 unsigned long finish=0;
-int flag=0;
+int flag=0,seat=0;
+int h=0,m=0;
+
 String working(float data) {
   if (10<data or -10>data){
     if(flag==0)
@@ -34,40 +38,49 @@ String working(float data) {
   if(flag==1)
    {
     finish=millis();
-    tabletime+=finish-start;
-
+    tabletime=finish-start;
+    //tabletime/=1000;
     flag=0;
     finish=0;
     start=0;
+    tabletime=0;
+    seat=1;
    }
  }
-tabletime/=1000;
+
+
+
 dtostrf(tabletime, 2, 2, scaleString);
+
+Serial.println(tabletime/1000);
 Serial.println(scaleString);
 Serial.println(data);
-return(String("field1=")+String(scaleString));
+return String(scaleString);
+//return(String("field1=")+String(scaleString));
 }
 void delivering(String payload) {
   WiFiClient client;
-  Serial.print("connecting to ");
-  Serial.println(host);
-  if (!client.connect(host, httpPort)) {
-    Serial.print("connection failed: ");
-    Serial.println(payload);
-    return;
-  }
-  String getheader = "GET "+ String(url) +"&"+ String(payload) +" HTTP/1.1";
-  client.println(getheader);
-  client.println("User-Agent: ESP8266");
-  client.println("Host: " + String(host));
-  client.println("Connection: close");
-  client.println();
+
+    Serial.print("\nconnecting to Host: ");
+  Serial.println(private_server);
+
+  //Connect to Private Server
+  if(client.connect(private_server,serverPort)){
+    //http call to server by using GET Method.
+    String getheader = "GET /?temp="+ String(payload) +" HTTP/1.1";
+    client.println(getheader);
+    client.println("User-Agent: ESP8266");  
+    client.println("Host: " + String(private_server));  
+    client.println("Connection: close");  
+    client.println();
   
-  Serial.println(getheader);
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    Serial.println(line);
-    }
+    Serial.println(getheader);//To Check
+    
+  }
+  else{
+    Serial.print("connection failed to ");
+    Serial.println(private_server);
+  }
     Serial.println("Done cycle.");
 }
 void connect_ap() {
@@ -93,9 +106,37 @@ void setup() {
 }
 unsigned long mark = 0;
 void loop() {
+  if(hour()==23&&minute()==59&&second()>55)
+  {
+    tabletime=0;
+    flag=0;
+  }
   if (millis() > mark) {
     mark+=interval;
     String payload = working(scale.get_units());
-    delivering(payload);
+    
+      if(minute()%5==0 && second()==0&&seat==0)
+      {
+        if(flag==0)
+         {
+            delivering(payload);  
+         }
+         else if (flag==1)
+         {
+          finish=millis();
+          tabletime=finish-start;
+          dtostrf(tabletime, 2, 2, scaleString);
+          //tabletime=0;
+          delivering(scaleString);
+          //start=millis();
+         }
+      }
+      if(seat==1)
+    {
+      delivering(payload);  
+      seat=0;
+    }
+      
     }
 }
+
